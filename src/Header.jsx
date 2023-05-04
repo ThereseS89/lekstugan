76bfb6c22632df22759c13fe069b91bc1b606363
Therese <thereseskold@live.com>
2023-05-04 19:55:19 +0200

    Gjorde klart designen i header och påbörjade designen i start komponenten

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,18 +2,32 @@ import logo from "../imgs/Lekstugan-Logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import './header.css'
 
 const Header = () => {
 	return (
 	<header>
-		<FontAwesomeIcon icon={faBars} />
+		
 		<div className="logo-container">
+			<FontAwesomeIcon id="hamburger-icon" icon={faBars} />
 			<img src={logo} />
+			<div className="spacer"></div>
+			<div className="cart">
+			<FontAwesomeIcon
+						icon={faCartShopping}
+						id="cart-icon" />
+						</div>
 		</div>
-		<FontAwesomeIcon
-						className="cart" 
-						icon={faCartShopping} />
+		<div className="searchBar">
+		<input
+		placeholder="Sök produkt"
+		className="search-input" />
+			<div className="search-icon"><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#000000",}} />
+			</div>
+		</div>
+		
+		
 	</header>
 	)
 }
--- src/routes/Start.jsx
+++ src/routes/Start.jsx
@@ -1,10 +1,34 @@
-
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Start = () => {
 
 	return (
-		<>
-		</>	
+		<section className="start">
+		<div 
+		className="comercial">
+			<h1 
+			className="comercial-text">20 - 50% på utvalda produkter </h1>
+		</div>
+		<div
+		className="first-page-info">
+			<h2 className="first-page-text">Kolla in sommarens leksaksfavoriter! <br /> Vi har produkter för både stora och små!</h2>
+		</div>
+		<div className="popular-text">
+					<p className="popular">Populärt just nu</p>
+				</div>
+		<div className="popular-container">
+				
+			<h3>AquaPlay</h3>
+			<img src="./imgs/aquaplay.png" />
+			<div className="price-container">
+				<p className="price">399 kr</p></div>
+			<div className="cart-container">
+				<FontAwesomeIcon
+						icon={faCartShopping} />
+			</div>
+		</div>
+		</section>	
 	)
 }
 
