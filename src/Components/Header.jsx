8a7caadbfc7dc90620ee27814fbf4a9f0b921489
Therese <thereseskold@live.com>
2023-05-14 20:30:57 +0200

    försöker publicera

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "/imgs/Lekstugan-Logo.png"
+import logo from "../imgs/Lekstugan-Logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
